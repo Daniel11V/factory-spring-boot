ea5635402d2af208edf5679ef270966ddf6ebb40
Daniel Vinet <daniel1vinet@gmail.com>
2022-02-02 19:06:31 -0300

    Using Docker

--- src/main/java/com/coderhouse/PatternsFactoryApplication.java
+++ src/main/java/com/coderhouse/PatternsFactoryApplication.java
@@ -1,10 +1,15 @@
 package com.coderhouse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 //@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+//@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @SpringBootApplication
 public class PatternsFactoryApplication {
 
